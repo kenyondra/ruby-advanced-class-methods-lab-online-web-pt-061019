c72664c035bf2c3d96a0f1a010475fe5a1e10fd5
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-14 02:07:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,7 @@ class Song
     def self.create_from_filename(filename)
       result = self.new_from_filename(filename)
       song = self.create 
-      song.name
+      song.name = result.name 
     end
     
     def self.destroy_all
